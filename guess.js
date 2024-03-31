02c0a5b884c82245f5f4a319ca1adf6f9a92d8c4
Pratiksha Singh <119301269+Pratiksha-287@users.noreply.github.com>
2024-03-31 12:43:32 +0530

    Add files via upload

---
+++ guess.js
@@ -0,0 +1,44 @@
+let a = Math.floor(Math.random() * 10) + 1;
+console.log(a);
+
+let chance = 5; //initialise chance outside the function
+
+function checkGuess() {
+  let n = parseInt(document.getElementById("guessInput").value);
+  let score = 0;
+
+  if (isNaN(n) || n < 1 || n > 10) {
+    document.getElementById("message").innerText =
+      "Please enter a valid number between 1 and 10.";
+    return;
+  }
+
+  if (n === a) {
+    score += 1;
+    document.getElementById("message").innerText =
+      "Congratulations! You guessed the correct number. Your score is: " +
+      score;
+
+    resetGame();
+  } else {
+    chance -= 1; // Decrease chance for each incorrect guess
+    document.getElementById("message").innerText =
+      "Incorrect guess. Try Again! You have " + chance + " more chances";
+    if (chance === 0) {
+      // Check if no more chances left
+      document.getElementById("message").innerText =
+        "Sorry, you've run out of chances. The correct number was " + a;
+      
+      resetGame();
+    }
+
+  }
+
+}
+
+function resetGame() {
+  a = Math.floor(Math.random() * 10) + 1;
+  console.log(a);
+  chance = 5; // Reset chance to 5
+  document.getElementById("guessInput").value = "";
+}
